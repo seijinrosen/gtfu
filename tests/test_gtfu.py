1418fa20d05363e55849378839fa85de8b1a361a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-08-05 07:20:42 +0900

    Add API #37

--- tests/test_gtfu.py
+++ tests/test_gtfu.py
@@ -1,5 +1,29 @@
-from gtfu import __version__
+from pytest import mark
+
+import gtfu
 
 
 def test_version():
-    assert __version__ == "1.1.0"
+    assert gtfu.__version__ == "1.1.0"
+
+
+@mark.parametrize(
+    ("url", "markdown", "expected"),
+    [
+        ("https://example_url", True, "[Example Title](https://example_url)"),
+        ("https://example_url", False, "Example Title"),
+    ],
+)
+def test_get(url: str, markdown: bool, expected: str):
+    assert gtfu.get(url, markdown) == expected
+
+
+@mark.parametrize(
+    ("url", "expected"),
+    [
+        ("https://example_url", "Example Title"),
+        ("example_url", "Example Title"),
+    ],
+)
+def test_get_only_url(url: str, expected: str):
+    assert gtfu.get(url) == expected
